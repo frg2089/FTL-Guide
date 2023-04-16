45eb61969e99be0f149b99b1615b92c784d04883
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-04-16 15:09:39 +0800

    :wrench: 更新配置
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- vuepress.theme.ts
+++ vuepress.theme.ts
@@ -12,15 +12,36 @@ export default hopeTheme({
   navbar,
   sidebar,
 
+  displayFooter: true,
   footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
 
   plugins: {
     mdEnhance: {
-      footnote: true,
-    },
-    copyright: {
-      global: true,
-      license: 'CC-BY-NC-SA 4.0'
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      container: true,
+      demo: true,
+      echarts: true,
+      figure: true,
+      flowchart: true,
+      gfm: true,
+      include: true,
+      katex: true,
+      mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      presentation: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+      },
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+      vuePlayground: true,
     }
   }
 })
